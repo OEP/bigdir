2c75c27003071ea5efd6fa059e23b4ca533ac17c
Paul Kilgo <paulkilgo@gmail.com>
2017-10-13 01:13:27 -0700

    Fix up unit tests

--- tests/test_bigdir.py
+++ tests/test_bigdir.py
@@ -1,13 +1,39 @@
 import pytest
 import bigdir
+import errno
 
 
-def test_scan(tmpdir):
+def test_scan_noarg():
     with pytest.raises(TypeError):
-        result = bigdir.scan()
+        bigdir.scan()
+
+
+def test_scan_too_many_args():
     with pytest.raises(TypeError):
-        result = bigdir.scan(None, None)
+        bigdir.scan(None, None)
+
+
+def test_scan_tmpdir(tmpdir):
     result = bigdir.scan(str(tmpdir))
     for item in result:
-        print item
-    assert False
+        assert item in ('.', '..')
+
+
+def test_scan_error_open(tmpdir):
+    f = tmpdir.join("test.txt")
+    f.write("Hello.")
+
+    # open() fails while initializing iterator
+    with pytest.raises(IOError) as e:
+        bigdir.scan(str(f))
+    assert e.value.errno == errno.ENOTDIR
+
+
+def test_scan_error_getdents(tmpdir):
+    # getdents() call fails in the middle of iteration
+    root = tmpdir.join('foo').mkdir()
+    iterator = bigdir.scan(str(root))
+    root.remove()
+    with pytest.raises(IOError) as e:
+        list(iterator)
+    assert e.value.errno == errno.ENOENT
